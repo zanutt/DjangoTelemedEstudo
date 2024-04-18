387433403d602b3b9654c069f952a82eafe6e0c9
zanutt <matszg@live.com>
2024-04-17 23:15:47 -0300

    dando continuidade no projeto de Telemed

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha =  request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
